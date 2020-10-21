5cb5f04d8c4b11971477e5b29543484bcc926b63
Ahmed <ahmed91abbas@hotmail.com>
2020-10-21 21:36:06 +0200

    feat: implement wikipedia data fetcher

---
+++ src/wikipedia_data_fetcher.py
@@ -0,0 +1,33 @@
+import re
+import wikipedia
+
+
+class Wikipedia_data_fetcher:
+    def get_article(self, article_name):
+        result = {}
+        try:
+            page = wikipedia.page(f'"{article_name}"')
+            result['name'] = page.title
+            result['url'] = page.url
+            result['content_chunks'] = self.chunk_page_content(page)
+        except wikipedia.exceptions.PageError:
+            pass
+        return result
+
+    def get_sections_titels(self, content):
+        return [x.group(1) for x in re.finditer('[\\n]+=+ +([A-Za-z ]+) +=+[\\n]+', content)]
+
+    def filter_sections(self, sections):
+        sections_to_remove = {'Publications', 'References', 'External links', 'Further reading'}
+        return [section for section in sections if section not in sections_to_remove]
+
+    def clean_text(self, text):
+        return text.replace('\"', '').replace('\n', ' ')
+
+    def chunk_page_content(self, page):
+        result = [self.clean_text(page.summary)]
+        sections = self.get_sections_titels(page.content)
+        filtered_sections = self.filter_sections(sections)
+        for section in filtered_sections:
+            result.append(self.clean_text(page.section(section)))
+        return result
---
+++ tests/wikipedia_data_fetcher_test.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import unittest
+import mock
+import wikipedia
+
+sys.path.append(os.path.abspath('..'))
+from wikipedia_data_fetcher import Wikipedia_data_fetcher
+
+
+class Test_wikipedia_data_fetcher(unittest.TestCase):
+
+    def setUp(self):
+        self.fetcher = Wikipedia_data_fetcher()
+
+    @mock.patch('wikipedia_data_fetcher.wikipedia.page')
+    @mock.patch.object(Wikipedia_data_fetcher, 'chunk_page_content')
+    def test_that_get_article_returns_correct_result(self, mock_chunk_page_content,
+                                                     mock_wikipedia_page):
+        url = 'fake url'
+        title = 'fake title'
+        content = 'fake content'
+        class Object:
+            def __init__(self):
+                self.url = url
+                self.title = title
+        mock_wikipedia_page.return_value = Object()
+        mock_chunk_page_content.return_value = content
+        expected =  {'name': title, 'url': url, 'content_chunks': content}
+
+        actual = self.fetcher.get_article('')
+
+        self.assertDictEqual(expected, actual)
+
+    @mock.patch('wikipedia_data_fetcher.wikipedia.page')
+    def test_that_get_article_returns_empty_dict_on_page_error(self, mock_wikipedia_page):
+        page_error = wikipedia.exceptions.PageError('fake page id')
+        mock_wikipedia_page.side_effect = page_error
+        expected =  {}
+
+        actual = self.fetcher.get_article('')
+
+        self.assertDictEqual(expected, actual)
+
+    def test_that_get_section_titles_strips_title_names_from_text(self):
+        content = '\n\n\n= First Title =\n text...\n\n\n== Second Title '\
+                  '==\n\n more text...\n=== Sub Title ===\n finish.'
+        expected = ['First Title', 'Second Title', 'Sub Title']
+
+        actual = self.fetcher.get_sections_titels(content)
+
+        self.assertListEqual(expected, actual)
+
+    def test_that_filter_sections_removes_unwanted_sections(self):
+        sections = ['Foo', 'Publications', 'References', 'External links', 'Further reading', 'Bar']
+        sections_to_remove = {'Publications', 'References', 'External links', 'Further reading'}
+        expected = ['Foo', 'Bar']
+
+        actual = self.fetcher.filter_sections(sections)
+
+        self.assertListEqual(expected, actual)
+
+    def test_that_clean_text_removes_quotes_and_newlines(self):
+        text = 'Some text\n"quoted text"\nend.'
+        expected = 'Some text quoted text end.'
+
+        actual = self.fetcher.clean_text(text)
+
+        self.assertEqual(expected, actual)
+
+    def test_that_chunk_page_content_returns_list_with_sections_contents(self):
+        summary = 'summary'
+        content = '\n= Foo =\nFirst chunk\n\n== Bar ==\nSecond chunk.'
+        expected = ['summary', 'First chunk', 'Second chunk.']
+        class Object:
+            def __init__(self):
+                self.summary = summary
+                self.content = content
+            def section(self, title):
+                if title == 'Foo':
+                    return 'First chunk'
+                elif title == 'Bar':
+                    return 'Second chunk.'
+
+        actual = self.fetcher.chunk_page_content(Object())
+
+        self.assertEqual(expected, actual)

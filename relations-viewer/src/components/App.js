3f18581f511d4f5285428af8aa127114ef566aa9
Ahmed <ahmed91abbas@hotmail.com>
2021-03-12 19:51:52 +0100

    feat: change components style

--- relations-viewer/src/components/App.js
+++ relations-viewer/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faEye, faEyeSlash, faEquals } from '@fortawesome/free-solid-svg-icons'
 
 import getRelations from '../api/relationsFinderApi'
 import { ResponsiveNeoGraph, showAll, stabilize, clearAll, hideAll, updateGraph } from './NeoGraph'
@@ -9,7 +11,10 @@ const NEO4J_USER = process.env.REACT_APP_NEO4J_USER
 const NEO4J_PASSWORD = process.env.REACT_APP_NEO4J_PASSWORD
 
 async function onSubmit (title) {
+  const element = document.getElementById('submitBtn')
+  element.classList.add('is-loading')
   const response = await getRelations(title)
+  element.classList.remove('is-loading')
   updateGraph(response.subject)
 }
 
@@ -22,18 +27,27 @@ function handleKeyPress (event) {
 const App = () => {
   const [input, setInput] = useState('')
   const infoPreId = 'infoPreId'
-  document.body.className = 'app'
   return (
-    <div className='app'>
-      <br /><br />
+    <div>
+      <pre className='bg' />
       <div className='padding'>
-        <input className='input' type='text' placeholder='Enter name' onKeyPress={handleKeyPress} onInput={e => setInput(e.target.value)} />
-        <button className='button submit-button' id='submitBtn' onClick={() => onSubmit(input)}>Submit</button>
-        <button className='button align-right' onClick={clearAll}>Clear all</button>
-        <button className='button align-right' onClick={showAll}>Show all</button>
-        <button className='button align-right' onClick={hideAll}>Hide all</button>
-        <button className='button align-right' onClick={stabilize}>Stabilize</button>
-        <pre />
+        <input className='input is-link is-medium input-custom' type='text' placeholder='Enter name' onKeyPress={handleKeyPress} onInput={e => setInput(e.target.value)} />
+        <button className='button is-medium is-link submit-button' id='submitBtn' onClick={() => onSubmit(input)}>Submit</button>
+        <div class='buttons are-medium align-right'>
+          <button class='button is-rounded is-dark' onClick={stabilize}>
+            <span class='icon'><i><FontAwesomeIcon icon={faEquals} /></i></span>
+          </button>
+          <button class='button is-rounded is-dark' onClick={showAll}>
+            <span class='icon'><i><FontAwesomeIcon icon={faEye} /></i></span>
+          </button>
+          <button class='button is-rounded is-dark' onClick={hideAll}>
+            <span class='icon'><i><FontAwesomeIcon icon={faEyeSlash} /></i></span>
+          </button>
+          <button class='button is-rounded is-dark' onClick={clearAll}>
+            <span class='icon'><i><FontAwesomeIcon icon={faTrash} /></i></span>
+          </button>
+        </div>
+        <pre className='bg' />
       </div>
       <ResponsiveNeoGraph
         containerId='neovis'
@@ -42,7 +56,7 @@ const App = () => {
         neo4jPassword={NEO4J_PASSWORD}
         infoPreId={infoPreId}
       />
-      <pre className='padding info' id={infoPreId} />
+      <pre className='padding info bg' id={infoPreId} />
     </div>
   )
 }
